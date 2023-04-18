69a6618e25f19027600edc528a5d1c82a89e26ba
IgneousRed <IgneousRed@protonmail.com>
2023-04-18 16:24:00 +0200

    snapshot

--- src/autoTest.zig
+++ src/autoTest.zig
@@ -12,23 +12,60 @@ const Random = rand.Random;
 
 pub fn testRNG(
     comptime RNG: type,
-    comptime orders: usize,
+    comptime maxOrder: usize,
     comptime threshold: f64,
-    // comptime runs: usize,
+    comptime runs: usize, // TODO: make runtime
+    config: RNG.Config,
+    alloc: Allocator,
+) !Score {
+    if (maxOrder < 10) return Score{ .order = 0, .fault = math.inf_f64 };
+
+    var runsOrdersSubs: [runs]BoundedArray(SubTests, maxOrder - 9) = undefined;
+    runLoop: for (runsOrdersSubs) |*run, r| {
+        var tester = TestDriver(RNG).init(@truncate(RNG.State, r), config, alloc);
+        defer tester.deinit();
+        while (run.len < run.buf.len) {
+            const subTests = try tester.next();
+            const w = try worst(subTests, alloc);
+            defer w.deinit();
+            if (w.fault >= threshold) {
+                subTests.deinit();
+                continue :runLoop;
+            }
+            run.addOneAssumeCapacity().* = subTests;
+        }
+    }
+    var ordersSubs
+    return Score{ .order = 0, .fault = math.inf_f64 };
+}
+
+pub fn worst(tests: SubTests, alloc: Allocator) !SubTest {
+    var iter = tests.iterator();
+    var result = iter.next() orelse unreachable;
+    while (true) {
+        const pair = iter.next() orelse break;
+        if (@fabs(pair.value_ptr.*) < @fabs(result.value_ptr.*)) result = pair;
+    }
+    return SubTest.init(result.key_ptr.*, result.value_ptr.*, alloc);
+}
+
+pub fn testRNG_OLD(
+    comptime RNG: type,
+    comptime maxOrder: usize,
+    comptime threshold: f64,
     config: RNG.Config,
     allocator: Allocator,
 ) !Score {
-    if (orders == 0) return Score{ .order = 0, .fault = math.inf_f64 };
+    if (maxOrder < 10) return Score{ .order = 0, .fault = math.inf_f64 };
 
     var driver = try TestDriver(RNG).init(0, config, allocator);
     defer driver.deinit();
-    var fault = 1 / @fabs((try driver.reportWorst()).result);
-    if (fault >= threshold) return Score{ .order = 0, .fault = fault };
-    var sum: f64 = 0;
-    var o: usize = 1;
-    while (o < orders) {
+    var sum = 1 / @fabs((try driver.reportWorst()).result);
+    if (sum >= threshold) return Score{ .order = 10, .fault = sum };
+    var o: usize = 11;
+    while (o < maxOrder) {
         defer o += 1;
-        fault = 1 / @fabs((try driver.reportWorst()).result);
+        const fault = 1 / @fabs((try driver.reportWorst()).result);
         if (sum + fault >= threshold) {
             break;
         } else sum += fault;
@@ -44,13 +81,12 @@ fn TestDriver(comptime RNG: type) type {
         writer: std.fs.File.Writer,
         readBuffer: []u8,
         writeBuffer: []RNG.Out,
-        alloc: Allocator,
 
-        readIndex: u16 = 0, // TODO: Refactor
+        readIndex: u16 = 0,
         writeReady: bool = false,
         subTests: SubTests = undefined,
 
-        pub fn init(testSeed: RNG.State, config: RNG.Config, allocator: Allocator) !Self {
+        pub fn init(seed: RNG.State, config: RNG.Config, alloc: Allocator) !Self {
             var tester = std.ChildProcess.init(&[_][]const u8{
                 "/Users/gio/PractRand/RNG_test",
                 "stdin",
@@ -65,7 +101,7 @@ fn TestDriver(comptime RNG: type) type {
                 "99",
                 "-tlmaxonly",
                 "-multithreaded",
-            }, allocator);
+            }, alloc);
             tester.stdin_behavior = .Pipe;
             tester.stdout_behavior = .Pipe;
             try tester.spawn();
@@ -73,28 +109,16 @@ fn TestDriver(comptime RNG: type) type {
             _ = try os.fcntl(tester.stdout.?.handle, os.F.SETFL, os.O.NONBLOCK);
 
             return Self{
-                .rng = RNG.init(testSeed, config),
+                .rng = RNG.init(seed, config),
                 .tester = tester,
                 .reader = tester.stdout.?.reader(),
                 .writer = tester.stdin.?.writer(),
-                .readBuffer = try allocator.alloc(u8, 1 << 20),
-                .writeBuffer = try allocator.alloc(RNG.Out, 1 << 10),
-                .alloc = allocator,
+                .readBuffer = try tester.allocator.alloc(u8, 1 << 20),
+                .writeBuffer = try tester.allocator.alloc(RNG.Out, 1 << 10),
             };
         }
 
-        pub fn reportWorst(self: *Self) !SubTest {
-            // defer self.subTests.deinit();
-            var iter = (try self.reportAll()).iterator();
-            var worst = iter.next() orelse unreachable;
-            while (true) {
-                const pair = iter.next() orelse break;
-                if (@fabs(pair.value_ptr.*) < @fabs(worst.value_ptr.*)) worst = pair;
-            }
-            return SubTest{ .name = try self.alloc.dupe(u8, worst.key_ptr.*), .result = worst.value_ptr.* };
-        }
-
-        pub fn reportAll(self: *Self) !SubTests {
+        pub fn next(self: *Self) !SubTests {
             self.subTests = SubTests.init(self.alloc);
             while (try self.read()) {
                 if (!self.writeReady) for (self.writeBuffer) |*w| {
@@ -108,6 +132,12 @@ fn TestDriver(comptime RNG: type) type {
             return self.subTests;
         }
 
+        pub fn deinit(self: *Self) void {
+            _ = self.tester.kill() catch unreachable;
+        }
+
+        // -------------------------------- Internal --------------------------------
+
         fn read(self: *Self) !bool {
             var readCount = self.reader.read(self.readBuffer[self.readIndex..]) catch 0;
             if (readCount == 0) return true;
@@ -116,23 +146,22 @@ fn TestDriver(comptime RNG: type) type {
 
             // Parse readBuffer
             while (self.readIndex < readLen) {
-                // If line not done
+                defer self.readIndex += 1;
                 if (self.readBuffer[self.readIndex] != '\n') {
-                    self.readIndex += 1;
                     continue;
                 }
                 if (try self.parseLine(self.readBuffer[lineStart..self.readIndex])) return false;
-                self.readIndex += 1;
-                lineStart = self.readIndex;
+                lineStart = self.readIndex + 1;
             }
 
             // Make space
             mem.copy(u8, self.readBuffer[0..], self.readBuffer[lineStart..readLen]);
             self.readIndex -= lineStart;
+            // std.debug.print("{s}\n", .{self.readBuffer[0..self.readIndex]});
             return true;
         }
 
-        fn parseLine(self: *Self, line: []const u8) bool {
+        fn parseLine(self: *Self, line: []const u8) !bool {
             // If line is empty
             if (line.len == 0) return self.subTests.count() > 0;
 
@@ -155,7 +184,7 @@ fn TestDriver(comptime RNG: type) type {
                 // When instead of 'p' there is "fail" or "pass"
                 if (line[l] == '"') {
                     const value: f64 = if (line[l + 1] == 'f') 0 else 0.5;
-                    try self.subTests.put(try self.alloc.dupe(u8, testName), value);
+                    try self.subTests.put(testName, value);
                     return false;
                 }
             }
@@ -172,7 +201,7 @@ fn TestDriver(comptime RNG: type) type {
 
             // If p == "nan"
             if (trailingChar == 'n') {
-                try self.subTests.put(try self.alloc.dupe(u8, testName), math.nan_f64);
+                try self.subTests.put(testName, math.nan_f64);
                 return false;
             }
 
@@ -198,7 +227,7 @@ fn TestDriver(comptime RNG: type) type {
             // If p == '0' or '1'
             if (nonDigitChar == ' ') {
                 const value = if (trailingNumber == 1) @as(f64, -0.0) else @as(f64, 0.0);
-                try self.subTests.put(try self.alloc.dupe(u8, testName), value);
+                try self.subTests.put(testName, value);
                 return false;
             }
 
@@ -206,7 +235,7 @@ fn TestDriver(comptime RNG: type) type {
             if (nonDigitChar == '.') {
                 var value = trailingNumber * math.pow(f64, 10, -trailingDigitCount);
                 if (value >= 0.5) value -= 1.0;
-                try self.subTests.put(try self.alloc.dupe(u8, testName), value);
+                try self.subTests.put(testName, value);
                 return false;
             }
 
@@ -222,15 +251,9 @@ fn TestDriver(comptime RNG: type) type {
 
             var value = coefficient * math.pow(f64, 10, -trailingNumber);
             if (line[symbolIndex] == '-') value = -value;
-            try self.subTests.put(try self.alloc.dupe(u8, testName), value);
+            try self.subTests.put(testName, value);
             return false;
         }
-
-        pub fn deinit(self: *Self) void {
-            _ = self.tester.kill() catch unreachable;
-        }
-
-        // -------------------------------- Internal --------------------------------
 
         const Self = @This();
     };
@@ -241,25 +264,64 @@ pub const Score = struct {
     fault: f64,
 
     pub fn worseThan(a: Score, b: Score) bool {
-        if (a.order == b.order) {
-            return if (a.fault > b.fault) true else false;
-        }
-        return if (a.order < b.order) true else false;
+        if (a.order == b.order) return a.fault > b.fault;
+        return a.order < b.order;
     }
-
-    // pub fn better(a: Score, b: Score) Score {
-    //     if (a.order == b.order) {
-    //         return if (a.fault < b.fault) a else b;
-    //     }
-    //     return if (a.order > b.order) a else b;
-    // }
 };
 
-pub const SubTests = StringHashMap(f64);
+pub const SubTests = ManagedStringHashMap(f64);
+
+pub fn ManagedStringHashMap(comptime T: type) type {
+    return struct {
+        map: StringHashMap(T),
+
+        pub fn init(alloc: Allocator) Self {
+            return Self{ .map = StringHashMap(T).init(alloc) };
+        }
+
+        pub fn count(self: *Self) Self.Size {
+            return self.map.count();
+        }
+
+        pub fn put(self: *Self, key: []const u8, value: T) !void {
+            if (self.map.contains(key)) {
+                self.map.putAssumeCapacity(key, value);
+                return;
+            }
+            self.map.putNoClobber(try self.map.allocator.dupe(u8, key), value);
+        }
+
+        pub fn deinit(self: *Self) void {
+            defer self.map.deinit();
+            var iter = self.map.keyIterator();
+            while (true) {
+                const key = iter.next() orelse break;
+                self.map.allocator.free(key); // deref?
+            }
+        }
+
+        // -------------------------------- Internal --------------------------------
+
+        const Self = @This();
+    };
+}
 
 pub const SubTest = struct {
     name: []const u8,
     result: f64,
+    alloc: Allocator,
+
+    pub fn init(name: []const u8, result: f64, alloc: Allocator) !Self {
+        return Self{ .name = try alloc.dupe(u8, name), .result = result, .alloc = alloc };
+    }
+
+    pub fn deinit(self: *Self) void {
+        self.alloc.free(self.name);
+    }
+
+    // -------------------------------- Internal --------------------------------
+
+    const Self = @This();
 };
 
 fn charToF64(char: u8) f64 {
